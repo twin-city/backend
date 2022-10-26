e3f106fef769e0e2d8d9c0f0f5203297fea8a629
victor <journe.victor@gmail.com>
2022-10-26 12:21:44 +0200

    Add endpoints

--- app/s3.py
+++ app/s3.py
@@ -13,33 +13,14 @@ class S3:
             region_name=None,
             endpoint=None):
         """
-        Create S3 Connector (with en vars of params)
-
-        Parameters
-        ----------
-        aws_access_key_id : str, optional
-            access key, by default None
-        aws_secret_access_key : str, optional
-            secret key, by default None
-        region_name : str, optional
-            region, by default None
-        endpoint : str, optional
-            endpoint, by default None
+        Create S3 Connector
         """
-        self.bucket = None if os.getenv(
-            'BUCKET_NAME') is None else os.getenv('BUCKET_NAME')
-        self.region_name = region_name if os.getenv(
-            'REGION') is None else os.getenv('REGION')
         self.endpoint = endpoint if os.getenv(
-            'ENDPOINT') is None else os.getenv('ENDPOINT')
-        aws_access_key_id = aws_access_key_id if os.getenv(
-            'ACCESS_KEY_ID') is None else os.getenv('ACCESS_KEY_ID')
-        aws_secret_access_key = aws_secret_access_key if os.getenv(
-            'ACCESS_KEY') is None else os.getenv('ACCESS_KEY')
+            'ENDPOINT') is not None else os.getenv('ENDPOINT','https://s3.fr-par.scw.cloud')
 
         self.client = boto3.client(
             's3',
-            region_name=self.region_name,
+            region_name=region_name,
             endpoint_url=self.endpoint,
             use_ssl=True,
             aws_access_key_id=aws_access_key_id,
@@ -66,14 +47,12 @@ class S3:
                     return [os.path.join(path, i) for i in os.listdir(path) if pathlib.Path(i).is_file()]
             return [path]
 
-    def upload(self, objet, recursive=False):
+    def upload(self, objet, recursive=False, bucket_name=None):
         filename = self._check_type(objet, recursive=recursive)
         for f in filename:
-            self.client.upload_file(Bucket=self.bucket,  Key=f,  Filename=f)
+            self.client.upload_file(Bucket=bucket_name,  Key=f,  Filename=f)
 
     def check_bucket(self, bucket_name=None):
-        if not bucket_name:
-            bucket_name = self.bucket
         try:
             self.client.head_bucket(Bucket=bucket_name)
             print("Bucket Exists!")
